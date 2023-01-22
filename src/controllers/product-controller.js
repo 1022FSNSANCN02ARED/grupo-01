0b5eeb270946b452feb8b4c0182e97955d516a79
Miguel101177 <mangelsany6@gmail.com>
2023-01-22 10:09:40 -0300

    Se modifica el listado de productos por errores productDataBase.json

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -1,23 +1,26 @@
-module.exports={
-    dashboard: (req, res) => {
-        res.render("dashboard/dashboard", {
-          //escritorio,
-        });
-      },
-      editarproducto: (req, res) => {
-        res.render("dashboard/editProduct", {
-          
-        });
-      },
+const products = require("../data/product");
 
-    productosadmin: (req, res) => {
-        res.render("dashboard/product", {
-          
-        });
-      },
-      newproduct: (req, res) => {
-        res.render("dashboard/newProduct", {
-          
-        });
-      },
+
+module.exports = {
+  dashboard: (req, res) => {
+    res.render("dashboard/dashboard", {
+      //escritorio,
+    });
+  },
+  editarproducto: (req, res) => {
+    res.render("dashboard/editProduct", {
+
+    });
+  },
+
+  productosadmin: (req, res) => {
+    res.render("dashboard/product", {
+
+    });
+  },
+  newproduct: (req, res) => {
+    res.render("dashboard/newProduct", {
+
+    });
+  },
 }
--- src/controllers/productController.js
+++
@@ -1,8 +0,0 @@
-const products = require("../data/product");
-
-const controller = {
-    // Root - Show all products
-    index: (req, res) => {
-      // Do the magic
-    },
-  };

c4924f1bf594bca6f35806edf59164f8ffc28756
Carvajal Maxi <108768085+carvajalmaxi@users.noreply.github.com>
2023-02-02 17:27:37 -0300

    Cambios en App

--- src/app.js
+++ src/app.js
@@ -1,20 +1,50 @@
-const express = require ("express");
-const path = require ("path");
+// ************ Require's ************
+const createError = require("http-errors");
+const cookieParser = require("cookie-parser");
+const express = require("express");
+const logger = require("morgan");
+const path = require("path");
+const methodOverride = require("method-override"); // Pasar poder usar los métodos PUT y DELETE
 
-const mainRouter = require("./routers/main-router");
+const viewUtils = require("./view-utils");
 
+// ************ express() - (don't touch) ************
 const app = express();
 
-app.use(mainRouter);
+// ************ Middlewares - (don't touch) ************
+app.use(express.static(path.join(__dirname, "../public"))); // Necesario para los archivos estáticos en el folder /public
+app.use(express.urlencoded({ extended: false }));
+app.use(logger("dev"));
+app.use(cookieParser());
+app.use(methodOverride("_method")); // Pasar poder pisar el method="POST" en el formulario por PUT y DELETE
 
-app.use(express.static(path.join(__dirname, "../public")));
+// ************ Template Engine - (don't touch) ************
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views")); // Define la ubicación de la carpeta de las Vistas
 
-const PORT = process.env.PORT || 3000;
+// ************ WRITE YOUR CODE FROM HERE ************
+// ************ Route System require and use() ************
+app.locals = viewUtils;
 
-app.listen(PORT, () => {
-    console.log(`Se prendió en el puerto ${PORT}`);
+const mainRouter = require("./routes/main"); // Rutas main
+
+app.use("/", mainRouter);
+
+// ************ DON'T TOUCH FROM HERE ************
+// ************ catch 404 and forward to error handler ************
+app.use((req, res, next) => next(createError(404)));
+
+// ************ error handler ************
+app.use((err, req, res, next) => {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.path = req.path;
+    res.locals.error = req.app.get("env") === "development" ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render("error");
 });
 
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "./views"));
-  
+// ************ exports app - dont'touch ************
+app.listen(3001);

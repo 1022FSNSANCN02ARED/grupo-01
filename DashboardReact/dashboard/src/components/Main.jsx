194ec9fe0db78a7b025ea725c1afc18ab41e03b2
Miguel101177 <mangelsany6@gmail.com>
2023-05-09 17:31:31 -0300

    Detalles del dashboard

--- DashboardReact/dashboard/src/components/Main.jsx
+++ DashboardReact/dashboard/src/components/Main.jsx
@@ -7,7 +7,6 @@ import { Route, Switch } from "react-router-dom";
 import MainContent from "./MainContent.jsx";
 import ProductList from "./ProductList";
 import ProductDetail from "./ProductDetail";
-import NewProduct from "./NewProduct";
 import UserDetail from "./UserDetail";
 import UserList from "./UserList";
 import Page404 from "../pages/page404.jsx";
@@ -15,39 +14,29 @@ import Page404 from "../pages/page404.jsx";
 function Main () {
   return (
 <Switch>
+  {/*Main*/}
     <Route path="/" exact>
-    
     <MainContent/>
-
     </Route>
-
+  
+  {/*Lista de productos*/}
     <Route path="/productList" >
-    
       <ProductList/>
-     
     </Route>
+
+  {/*Detalle de productos*/} 
     <Route path="/productDetail/:id"  component={ProductDetail}/>
-    
-    <Route path="/newProduct" >
-    
-      <NewProduct/>
-     
-    </Route>
-      
-    {/* <Route path="/EditProduct/id" >
-    
-      <EditProduct/>
-     
-    </Route> */}
+
+  {/*Lista de usuarios*/}  
     <Route path="/UserList" >
-    
-      <UserList/>
-     
+      <UserList/> 
     </Route>
+
+  {/*Detalle de usuario*/}
     <Route path="/userDetail/:id"  component={UserDetail}/>
 
+  {/*Página no encontrada*/}
     <Route path="*" component={Page404} />
-
 
 </Switch>
   );

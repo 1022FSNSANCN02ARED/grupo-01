9e225f0f266388558d1f4dee3356528659ea9e3f
Nataia-3105 <natalia.lange1801@gmail.com>
2023-05-10 21:19:37 -0300

    se corrije codigo validaciones products

--- src/middlewares/resultadoValidacionesCreateProduct.js
+++ src/middlewares/resultadoValidacionesCreateProduct.js
@@ -1,17 +1,38 @@
+const Sequelize= require ('sequelize');
+const { Product, Brand, Colors, Genre, Sizes, Category, Images, ProductSizes, ProductColors, Material } = require("../database/models");
 const { validationResult }=require("express-validator");
 
 
-    function resultvalidations(req,res,next){ 
+async function  resultvalidations  (req,res,next)  { 
         
         const resultvalidations=validationResult(req);
         const oldValues=req.body;
         const oldValuesFiles=req.files;
+
+        let product=await Product.findAll();
+        let marca=await Brand.findAll();
+        let colors=await Colors.findAll();
+        let genre=await Genre.findAll();
+        let sizes=await Sizes.findAll();
+        let category=await Category.findAll();
+        let material=await Material.findAll();  
+        let images=await Images.findAll();  
+
+        console.log(oldValues);
     
         if (resultvalidations.errors.length>0) {
             return  res.render("dashboard/newProduct",{
             errors:resultvalidations.mapped(),
             oldValues:oldValues,
             product:oldValues,
+            product : product,
+            category: category,
+            brand: marca,
+            colors: colors,
+            genre: genre,
+            material: material,
+            sizes: sizes,
+            images: images,
             oldValuesFiles
              });   
         }else{
